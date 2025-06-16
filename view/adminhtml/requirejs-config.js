45da29f927cf1714d7cde1b846d534c7b4fa9296
Piotr Matras <piotr.matras@creativestyle.pl>
2025-06-16 16:42:57 +0200

    feat: MOV-240 add svg+webp image extension

---
+++ view/adminhtml/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Ui/js/form/element/image-uploader': {
+                'MageSuite_FileUpload/js/form/element/image-uploader-mixin': true
+            }
+        }
+    }
+};

981ffd3c6f1e61957562e4f45f103cbcc64a8abc
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2025-09-02 08:42:21 +0200

    fix: [IPET-3180] Fix for wrong variable type

--- Plugin/Magento/Framework/Image/Adapter/AbstractAdapter/DisableImageProcessingForNonImageFiles.php
+++ Plugin/Magento/Framework/Image/Adapter/AbstractAdapter/DisableImageProcessingForNonImageFiles.php
@@ -8,11 +8,12 @@ class DisableImageProcessingForNonImageFiles
 {
     public function __construct(
         protected array $nonImageFilesExtensions
-    ) {}
+    ) {
+    }
 
-    public function aroundOpen(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $filename): void // phpcs:ignore
+    public function aroundOpen(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $filename): void //phpcs:ignore
     {
-        $extension = pathinfo($filename, PATHINFO_EXTENSION); // phpcs:ignore
+        $extension = pathinfo($filename, PATHINFO_EXTENSION); //phpcs:ignore
 
         if (in_array($extension, $this->nonImageFilesExtensions)) {
             return;
@@ -21,9 +22,14 @@ class DisableImageProcessingForNonImageFiles
         $proceed($filename);
     }
 
-    public function aroundSave(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $destination = null, $newName = null): void // phpcs:ignore
+    public function aroundSave(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $destination = null, $newName = null): void //phpcs:ignore
     {
-        $extension = pathinfo($destination, PATHINFO_EXTENSION); // phpcs:ignore
+        if (empty($destination)) {
+            $proceed($destination, $newName);
+            return;
+        }
+
+        $extension = pathinfo($destination, PATHINFO_EXTENSION); //phpcs:ignore
 
         if (in_array($extension, $this->nonImageFilesExtensions)) {
             return;
@@ -32,7 +38,7 @@ class DisableImageProcessingForNonImageFiles
         $proceed($destination, $newName);
     }
 
-    public function aroundResize(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $width = null, $height = null): void // phpcs:ignore
+    public function aroundResize(\Magento\Framework\Image\Adapter\AbstractAdapter $subject, callable $proceed, $width = null, $height = null): void //phpcs:ignore
     {
         if (empty($subject->getFileSrcName())) {
             return;
